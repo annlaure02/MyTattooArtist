a53faa614bfc03aa890979a9cd001e64ee90a3f5
CllaudiaB <buraanna27@gmail.com>
2023-06-12 13:53:31 +0200

    update name field studio_number_street in the class Meta

--- my_tattoo_artist/backend/user_artist/serializers.py
+++ my_tattoo_artist/backend/user_artist/serializers.py
@@ -7,7 +7,7 @@ class UserArtistSerializer(serializers.ModelSerializer):
     tattoo_style = TattooStyleSerializer(many=True, required=False)
     class Meta:
         model = UserArtist
-        fields = ['first_name', 'last_name', 'id', 'artist_name', 'phone', 'email', 'profile_picture', 'biography', 'album', 'drawing', 'studio_name', 'studio_number_stree', 'studio_street', 'studio_city', 'studio_post_code', 'studio_country', 'studio_state', 'tattoo_style']
+        fields = ['first_name', 'last_name', 'id', 'artist_name', 'phone', 'email', 'profile_picture', 'biography', 'album', 'drawing', 'studio_name', 'studio_number_street', 'studio_street', 'studio_city', 'studio_post_code', 'studio_country', 'studio_state', 'tattoo_style']
         extra_kwargs = {'password': {'write_only': True, 'required': False},
                         'email': {'required': False},
         }
