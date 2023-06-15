b5f5727aebf2e86c20d9a5092bbbc9415ed9bcf5
CllaudiaB <buraanna27@gmail.com>
2023-06-15 12:02:35 +0200

    added method update in file seryalizer.py

--- my_tattoo_artist/backend/user_artist/serializers.py
+++ my_tattoo_artist/backend/user_artist/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import UserArtist
 from project.serializers import TattooStyleSerializer
+from project.models import TattooStyle
 
 
 class UserArtistSerializer(serializers.ModelSerializer):
@@ -17,3 +18,16 @@ class UserArtistSerializer(serializers.ModelSerializer):
         
         extra_kwargs = {'password': {'write_only': True, 'required': False},
                         'email': {'required': False}}
+        
+
+    def update(self, instance, validated_data):
+        tattoo_data = validated_data.pop('tattoo_style', None)
+        instance = super().update(instance, validated_data)
+        tattoo_obj = []
+        if tattoo_data is not None:
+            instance.tattoo_style.clear()
+            for tattoo_style_data in tattoo_data:
+                tattoo, created = TattooStyle.objects.get_or_create(**tattoo_style_data)
+                tattoo_obj.append(tattoo)
+            instance.tattoo_style.set(tattoo_obj)
+        return instance
