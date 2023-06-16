5c00ae0bed04aa14c49b54552e4e59bf25cd6ff8
CllaudiaB <buraanna27@gmail.com>
2023-06-16 20:27:08 +0200

    added usUserArtistAlbum and UserArtistDrawing in the file admin.py

--- my_tattoo_artist/backend/user_artist/admin.py
+++ my_tattoo_artist/backend/user_artist/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserArtist
+from .models import UserArtist, UserArtistAlbum, UserArtistDrawing
 
 # Register your models here.
 
 admin.site.register(UserArtist)
+admin.site.register(UserArtistAlbum)
+admin.site.register(UserArtistDrawing)
