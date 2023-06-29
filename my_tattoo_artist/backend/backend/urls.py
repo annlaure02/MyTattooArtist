ff1070a95cd0e40f10f2a05a77bb8cf3ebafd43c
CllaudiaB <buraanna27@gmail.com>
2023-06-29 11:48:33 +0200

    added path admin in urls.py

--- my_tattoo_artist/backend/backend/urls.py
+++ my_tattoo_artist/backend/backend/urls.py
@@ -20,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
     path('user_artist/', include('user_artist.urls')),
-    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
